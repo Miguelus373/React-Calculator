f1bb9dbbae7bcd5cac1cd33fec51933befc3a3ce
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-02-04 12:42:43 -0400

    Refactor ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,37 +1,25 @@
 import Button from './Button';
 
-const ButtonPanel = () => (
-  <>
-    <div>
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" />
-    </div>
-    <div>
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" />
-    </div>
-    <div>
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" />
-    </div>
-    <div>
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" />
-    </div>
-    <div>
-      <Button name="0" />
-      <Button name="." />
-      <Button />
-    </div>
-  </>
-);
+const ButtonPanel = () => {
+  const buttons = {
+    group1: ['AC', '+/-', '%', '÷'],
+    group2: ['7', '8', '9', 'X'],
+    group3: ['4', '5', '6', '-'],
+    group4: ['1', '2', '3', '+'],
+    group5: ['0', '.', '='],
+  };
+
+  return (
+    <>
+      {Object.keys(buttons).map(group => (
+        <div key={group}>
+          {buttons[group].map(button => (
+            <Button key={button} name={button} />
+          ))}
+        </div>
+      ))}
+    </>
+  );
+};
 
 export { ButtonPanel as default };
