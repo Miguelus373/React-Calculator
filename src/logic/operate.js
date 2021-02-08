72015653a87c212e50ac11bc1e6e09cf539fda55
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-02-08 18:10:15 -0400

    Refactor calculate & operate

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,33 +1,36 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
+  if (numberOne === null || numberTwo === null) {
+    return 0;
+  }
+
   let result = 0;
   const x = Big(numberOne);
   const y = Big(numberTwo);
 
   switch (operation) {
     case '+':
-      result = x + y;
+      result = (parseFloat(x) + parseFloat(y)).toString();
       break;
 
     case '-':
       result = x - y;
       break;
 
-    case '*':
+    case 'X':
       result = x * y;
       break;
 
-    case '/':
+    case '÷':
       result = x / y;
       break;
 
     default:
-      result /= 100;
       break;
   }
 
-  return result;
+  return (result === Infinity ? 'Error' : result);
 };
 
 export { operate as default };
