7b2e52d35181a42e3852d1b51c08003ab36ffe4c
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-02-12 16:34:31 -0400

    Remove commented code

--- src/components/App.js
+++ src/components/App.js
@@ -27,29 +27,3 @@ const App = () => {
 };
 
 export { App as default };
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(buttonName) {
-//     this.setState(previous => calculate(previous, buttonName));
-//   }
-
-//   render() {
-//     const { total, next } = this.state;
-
-//     return (
-//       <>
-//         <Display result={next ?? total} />
-//         <ButtonPanel clickHandler={this.handleClick} />
-//       </>
-//     );
-//   }
-// }
