3b208ca64e0d556b4ca44c4f3caccd617c13c0d9
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-02-16 16:00:06 -0400

    Add quote component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Calculator from './Calculator';
 import Navbar from './Navbar';
 import Home from './Home';
+import Quote from './Quote';
 
 const App = () => (
   <main>
     <Navbar />
     <Switch>
       <Route path="/calculator" component={Calculator} />
-      {/* <Route path="/quote" component={Quote} /> */}
+      <Route path="/quote" component={Quote} />
       <Route path="/" component={Home} />
     </Switch>
   </main>
